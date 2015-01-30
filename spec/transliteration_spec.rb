883bb716117adb61c8ff30972cf76b8060537b81
Andrew Volozhanin <linuxheadrus@gmail.com>
2015-01-30 16:37:32 +0500

    Add detransliteration from Russian to English

--- spec/transliteration_spec.rb
+++ spec/transliteration_spec.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*- 
+# -*- encoding: utf-8 -*-
 
 require File.dirname(__FILE__) + '/spec_helper'
 
@@ -8,10 +8,22 @@ describe Russian do
       Russian::transliterate(str)
     end
 
+    def dt(str)
+      Russian::Transliteration::detransliterate(str)
+    end
+
     %w(transliterate translit).each do |method|
       it "'#{method}' method should perform transliteration" do
         str = mock(:str)
         Russian::Transliteration.should_receive(:transliterate).with(str)
+        Russian.send(method, str)
+      end
+    end
+
+    %w(detransliterate detranslit).each do |method|
+      it "'#{method}' method should perform de-transliteration" do
+        str = mock(:str)
+        Russian::Transliteration.should_receive(:detransliterate).with(str)
         Russian.send(method, str)
       end
     end
@@ -26,13 +38,27 @@ describe Russian do
       t("ш").should == "sh"
       t("Ш").should == "SH"
       t("ц").should == "ts"
+      # t("схема").should == "skhema"
     end
-    
+
+    it "should de-transliterate properly" do
+      dt("Eto prosto nekiy tekst").should == "Ето просто некий текст"
+      dt("sch").should == "щ"
+      dt("Zveryo moyo").should == "Зверё моё"
+      dt("mayskiy izmenyaya moey pamyatyu tvoeyu pohodkoyu vyshla iz maya ob'ektoy matyoy").should == "майский изменяя моей памятю твоею походкою вышла из мая обЪектой матёй"
+      dt("IE explorer").should == "ИЕ експлорер"
+      dt("upuscheniy").should == "упущений"
+      dt("sh").should == "ш"
+      dt("TS").should == "Ц"
+      dt("skhema").should == "схема"
+      dt("Tyumen").should == "Тюмень"
+    end
+
     it "should properly transliterate mixed russian-english strings" do
-      t("Это кусок строки русских букв v peremeshku s latinizey i амперсандом (pozor!) & something").should == 
-        "Eto kusok stroki russkih bukv v peremeshku s latinizey i ampersandom (pozor!) & something"      
+      t("Это кусок строки русских букв v peremeshku s latinizey i амперсандом (pozor!) & something").should ==
+        "Eto kusok stroki russkih bukv v peremeshku s latinizey i ampersandom (pozor!) & something"
     end
-    
+
     it "should properly transliterate mixed case chars in a string" do
       t("НЕВЕРОЯТНОЕ УПУЩЕНИЕ").should == "NEVEROYATNOE UPUSCHENIE"
       t("Невероятное Упущение").should == "Neveroyatnoe Upuschenie"
